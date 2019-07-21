280910da82125fcfbf49c07f9ae71681c6a48700
alan <leftwo@gmail.com>
2019-07-21 15:09:46 -0700

    Upated pfi to be all in the object.
    Moved almost everythign into the ddd object.  This object has a terrible
    name, but untill I can think of a better one, I'm going to leave it
    terrible.  If I come up with an almost good enough name, I'll never
    change it, and that's worse.

--- pfi.py
+++ pfi.py
@@ -7,42 +7,6 @@ import random
 import sys
 import argparse
 
-def add_fuel(fuel, filename):
-    """ Read in a file and build the fuel matrix based on the file contents
-    """
-
-    width = fuel.shape[0]
-    height = fuel.shape[1]
-    with open(filename) as f:
-        content = f.readlines()
-        content = [x.strip() for x in content] 
-
-    y = 0
-    for line in content:
-        x_max = min(width, len(line))
-        for x in range(x_max):
-            fuel[x][y] = int(ord(line[x]))
-
-        y += 1
-        if y > height:
-            break
-
-
-def set_color_temp(x, y, fuel, temp):
-    if temp[x][y] > 100:
-        return 4
-
-    if temp[x][y] > 75:
-        return 3
-
-    if temp[x][y] > 50:
-        return 2
-
-    if temp[x][y] > 25:
-        return 1
-
-    return 0
-
 def set_color(x, y, fuel, temp):
     """ Temperature based color setting """
 
@@ -67,94 +31,127 @@ def set_color(x, y, fuel, temp):
 
     return 0
 
+class ddd(object):
+    """ Needs a better name """
+    def __init__(self):
+        self.screen  = curses.initscr()
+        self.width   = self.screen.getmaxyx()[1]
+        self.height  = self.screen.getmaxyx()[0]
 
-def fire(stdscr, filename):
+        self.fuel = np.full((self.width, self.height), 32)
+        self.temp = np.full((self.width, self.height), 0)
 
-    screen  = curses.initscr()
-    width   = screen.getmaxyx()[1]
-    height  = screen.getmaxyx()[0]
-    color   = 3
+        self.temp[0][2] = 3
+        self.temp[2][5] = 5
 
-    fuel = np.full((width, height), 32)
-    temp = np.full((width, height), 3)
+        curses.curs_set(0)
+        curses.start_color()
+        curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
+        curses.init_pair(2,curses.COLOR_MAGENTA,curses.COLOR_BLACK)
+        curses.init_pair(3,curses.COLOR_RED,curses.COLOR_BLACK)
+        curses.init_pair(4,curses.COLOR_BLUE,curses.COLOR_BLACK)
+        self.screen.clear
 
-    add_fuel(fuel, filename)
+    def add_fuel(self, filename):
+        """ Read in a file and build the fuel matrix based on the file contents
+        """
+        with open(filename) as f:
+            content = f.readlines()
+            content = [x.strip() for x in content] 
 
-    temp[0][2] = 3
-    temp[2][5] = 5
-    curses.curs_set(0)
-    curses.start_color()
-    curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
-    curses.init_pair(2,curses.COLOR_MAGENTA,curses.COLOR_BLACK)
-    curses.init_pair(3,curses.COLOR_RED,curses.COLOR_BLACK)
-    curses.init_pair(4,curses.COLOR_BLUE,curses.COLOR_BLACK)
-    screen.clear
+        y = 0
+        for line in content:
+            x_max = min(self.width, len(line))
+            for x in range(x_max):
+                self.fuel[x][y] = int(ord(line[x]))
 
-    while temp.any() != 0:
-        change = np.full((width, height), 0)
+            y += 1
+            if y > self.height:
+                break
 
-        for y in range(0, height):
-            for x in range(0, width):
-                # How fast we go through the fuel
-                if random.randrange(2) == 0:
-                    fire_check(x, y, temp, fuel, change)
+    def show_screen(self):
+        for x in range(0, self.width - 1):
+            for y in range(0, self.height - 1):
 
-        temp = np.maximum(change, temp)
-
-        for x in range(0, width - 1):
-            for y in range(0, height - 1):
-
-                color = set_color(x, y, fuel, temp)
+                color = set_color(x, y, self.fuel, self.temp)
 
                 # Print the character
-                my_chr = chr(fuel[x][y])
-                screen.addstr(y, x, my_chr, curses.color_pair(color) | curses.A_BOLD )
+                my_chr = chr(self.fuel[x][y])
+                self.screen.addstr(y, x, my_chr, curses.color_pair(color) | curses.A_BOLD )
                 # Print the left most digit of the temperature.
                 # screen.addstr(y, x, str(temp[x][y])[0], curses.color_pair(color) | curses.A_BOLD )
 
-        screen.refresh()
-        screen.timeout(30)
+        self.screen.refresh()
+        self.screen.timeout(30)
 
-        if screen.getch() != -1:
-            break
+    def key_press(self):
+        """ Check to see if a curses keypress was detected """
+        self.screen.nodelay(True)
+        return self.screen.getch()
 
+    def burn_step(self):
+        """ Run every locaiton through a burn step, update
+            the surrounding spots with the new temp. """
+        change = np.full((self.width, self.height), 0)
+        for x in range(0, self.width - 1):
+            for y in range(0, self.height - 1):
+                # How fast we go through the fuel
+                if random.randrange(2) == 0:
+                    self.fire_check_point(x, y, change)
 
-def fire_check(x, y, temp, fuel, change):
-    width = fuel.shape[0]
-    height = fuel.shape[1]
+        self.temp = np.maximum(change, self.temp)
 
-    if fuel[x][y] > 32:
-       if temp[x][y] > 2:
-            temp[x][y] += 1
-            fuel[x][y] = fuel[x][y] - 1
-       else:
-            temp[x][y] = max(0, temp[x][y] - 1)
+    def fire_check_point(self, x, y, change):
 
-    # Empty cells drain heat faster than full
-    else:
-        temp[x][y] = int(temp[x][y] / 2)
+        if self.fuel[x][y] > 32:
+           if self.temp[x][y] > 2:
+                self.temp[x][y] += 1
+                self.fuel[x][y] = self.fuel[x][y] - 1
+           else:
+                self.temp[x][y] = max(0, self.temp[x][y] - 1)
 
-    temp[x][y] = min(200, temp[x][y])
-
-    if temp[x][y] > 5:
-        # How hot we are defines how much heat we spread
-        # If we have no fuel, then we are just passing heat
-        # from other cells, so we further reduce heat spread
-        if fuel[x][y] > 32:
-            heat_spread = int(temp[x][y] * 0.90)
+        # Empty cells drain heat faster than full
         else:
-            heat_spread = int(temp[x][y] * 0.75)
+            self.temp[x][y] = int(self.temp[x][y] / 2)
 
-        # We can't expect a cell next to us to get hotter than
-        # we are currently.
-        if x > 0 and x < width - 1:
-            change[x - 1][y] = min(200, max(change[x - 1][y], heat_spread))
-        if x < (width - 1) and x >= 0:
-            change[x + 1][y] = min(200, max(change[x + 1][y], heat_spread))
-        if y > 0 and y < height:
-            change[x][y - 1] = min(200, max(change[x][y - 1], heat_spread))
-        if y < (height - 1) and y >= 0:
-            change[x][y + 1] = min(200, max(change[x][y + 1], heat_spread))
+        self.temp[x][y] = min(200, self.temp[x][y])
+
+        if self.temp[x][y] > 5:
+            # How hot we are defines how much heat we spread
+            # If we have no fuel, then we are just passing heat
+            # from other cells, so we further reduce heat spread
+            if self.fuel[x][y] > 32:
+                heat_spread = int(self.temp[x][y] * 0.90)
+            else:
+                heat_spread = int(self.temp[x][y] * 0.75)
+
+            # We can't expect a cell next to us to get hotter than
+            # we are currently.
+            if x > 0 and x < self.width - 1:
+                change[x - 1][y] = min(200, max(change[x - 1][y], heat_spread))
+            if x < (self.width - 1) and x >= 0:
+                change[x + 1][y] = min(200, max(change[x + 1][y], heat_spread))
+            if y > 0 and y < self.height:
+                change[x][y - 1] = min(200, max(change[x][y - 1], heat_spread))
+            if y < (self.height - 1) and y >= 0:
+                change[x][y + 1] = min(200, max(change[x][y + 1], heat_spread))
+
+def fire(stdscr, filename):
+    """ Main loop for display."""
+    my_dis = ddd()
+    my_dis.add_fuel(filename)
+    my_dis.show_screen()
+
+    while True:
+        command = my_dis.key_press()
+        if command != -1:
+            break
+        my_dis.burn_step()
+        my_dis.show_screen()
+
+        # if temp.any() == 0:
+        #     break
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
